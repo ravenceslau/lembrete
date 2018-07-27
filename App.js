5e52aa1c932fb0d99121220938de3f160e0a8f59
Robson Venceslau <ravences@gmail.com>
2018-07-26 23:19:46 -0300

    Modificado o App.js

--- App.js
+++ App.js
@@ -1,39 +1,10 @@
-import React from 'react'
-import { Text, View, Button } from 'react-native'
+import React, { Component } from 'react'
+import ListaLembretes from './ListaLembretes'
 
 export default class App extends React.Component {
-  // array de lembretes
-  state = {
-    lembretes: []
-  }
-
-  // método responsável por acessar a api e receber os dados em json
-  componentDidMount(){
-    fetch('https://devreminder.herokuapp.com/lembrete', {method: 'GET'})
-      .then(T => T.json())
-      .then(lembretes => this.setState({ lembretes }))
-  }
-
   render() {
-
-    const { lembretes } = this.state
-
     return (
-      <View>
-        <Text>Lembrete</Text>
-        <Button title='Adicionar' onPress={()=> console.log('criou o lembrete')} />
-
-        <View>
-          { lembretes.map((lembrete, key) => (
-            <View key={key}>
-              <Text>{lembrete.conteudo}</Text>
-              <Button title='Editar' onPress={()=> console.log('editar lemrete')} />
-              <Button title='Excluir' onPress={()=> console.log('excluir lemrete')} />
-            </View>
-          ))}
-        </View>
-      </View>
+      <ListaLembretes />
     )
   }
-}
-
+} 
